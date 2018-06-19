7571e32f779c4b530c94b823c9ffbf0a8984fc03
wangweibin1 <wangweibin1@360.cn>
2018-06-19 21:21:27 +0800

    update

--- nawa/src/resource/js/xingyu.js
+++ nawa/src/resource/js/xingyu.js
@@ -1,4 +1,4 @@
-import '../css/bodybuilding.scss';
+import '../css/diet.scss';
 import '../css/swiper.css';
 
 const util = require('./common/util');
@@ -9,25 +9,9 @@ let data = [{
     "img": "resource/img/banner.jpg",
     "url": "#1",
     "title": "1111111111111"
-},{
-    "img": "resource/img/banner.jpg",
-    "url": "#2",
-    "title": "222222222"
 }]
 function bindEnds(){
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-		// 获取已激活的标签页的名称
-		var activeTab = $(e.target).text(); 
-		// 获取前一个激活的标签页的名称
-		var previousTab = $(e.relatedTarget).text();
-	});
-	let $buildingFixed = $(".building-fixed");
-	$(".coach-ul").on("click", "li", function(){
-		$buildingFixed.show();
-	})
-	$buildingFixed.on("click", ".close", function(){
-		$buildingFixed.hide();
-	})
+
 }
 function banner(){
     var html = "";
@@ -35,7 +19,7 @@ function banner(){
 		html += '<div class="swiper-slide"><a href="'+ data[i].url +'" style="background-image:url('+ data[i].img +');background-size:cover;background-position:center center;" title="'+ data[i].title +'"></a></div>';
 	}
 	$(".swiper-wrapper").html(html);
-	seiper()	
+	seiper()
 }
 function seiper(){
 	var swiper = new Swiper('.swiper-container', {
