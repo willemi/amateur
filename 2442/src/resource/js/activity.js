b232298bd5af2d8143e1589235a64197c09b26aa
wangweibin1 <wangweibin1@360.cn>
2018-07-18 19:37:27 +0800

    update

--- 2442/src/resource/js/activity.js
+++ 2442/src/resource/js/activity.js
@@ -1,7 +1,27 @@
 import '../css/activity.scss';
 
-const util = require('./common/util');
+import '../css/swiper.min.css';
+const Swiper = require('../js/common/swiper.min');
 
+const util = require('./common/util');
+var swiper = new Swiper('.swiper-container',{
+	autoplay:3000,
+	speed:1000,
+	autoplayDisableOnInteraction : false,
+	loop:true,
+	centeredSlides : true,
+	slidesPerView:2,
+	prevButton:'.swiper-button-prev',
+	nextButton:'.swiper-button-next',
+	onInit:function(swiper){
+		swiper.slides[2].className="swiper-slide swiper-slide-active";//第一次打开不要动画
+	},
+	breakpoints: { 
+		668: {
+			slidesPerView: 1,
+		}
+	}
+});
 function bindEnds(){
 
 }
--- 2442/src/resource/js/exhibition.js
+++ 2442/src/resource/js/exhibition.js
@@ -1,3 +1,18 @@
 import '../css/exhibition.scss'
 
+import '../css/swiper.css';
+const Swiper = require('../js/common/swiper.min');
+
 const util = require('./common/util');
+
+var mySwiper = new Swiper('.swiper-container', {
+    // autoplay: true,//可选选项，自动滑动
+    pagination: '.s-pagination',
+    paginationClickable:true,
+    // 自定义分页器，必须的type类型
+    paginationBulletRender: function (swiper, index, className) {
+        return '<li class="' + className + '"><img src="resource/img/70_100.jpg"><p>某某跑团某某跑团某某跑团</p></li>'
+    },
+    nextButton: '.s-btn-next',
+    prevButton: '.s-btn-pre'
+})
