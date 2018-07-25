f131bb5b930e9a88ebc6d4175252286edc9f1763
willemi <w.wbin@foxmail.com>
2018-07-25 22:52:29 +0800

    update

---
+++ 2442/src/resource/js/guinness-signUp.js
@@ -0,0 +1,11 @@
+import '../css/guinness-signUp.scss';
+
+const util = require('./common/util');
+
+function bindEnds(){
+
+}
+function init(){
+	bindEnds();
+}
+init()
---
+++ dadi/src/resource/js/system-Journal.js
@@ -0,0 +1,12 @@
+import '../css/system-Journal.scss';
+
+const util = require("./common/util.js")
+
+function bindEvents(){
+    var $doc = $(document);
+    
+}
+function init(){
+	bindEvents();
+}
+init();
